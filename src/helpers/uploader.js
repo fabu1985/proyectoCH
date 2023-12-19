a078f72c62cf904d2384084e1c589bab16b13a5f
Fabiana Diaz <fabianadiazp@gmail.com>
2023-12-19 20:16:59 -0300

    chat funcionando

--- src/helpers/uploader.js
+++ src/helpers/uploader.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
-const { dirname } = require('node:path');
+const { dirname } = require('path');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
