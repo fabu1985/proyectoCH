306ac4a6c67e06558cd56c1311f1e0d78b75ef1a
Fabiana Diaz <fabianadiazp@gmail.com>
2023-12-28 22:53:52 -0300

    funciona product y carritos ok

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -3,14 +3,6 @@ const { productsModel, usersModel } = require('../dao/mongo/models/ecommerce.mod
 const { authentication } = require('../middlewares/auth.middleware')
 const router = Router()
 
-const productMock = [
-    {
-        id: 1, title: 'Product1', price: 22323, stock: 2, description: 'alguna descripcion',
-        id: 2, title: 'Product2', price: 22323, stock: 4, description: 'alguna descripcion2',
-        id: 3, title: 'Product3', price: 22323, stock: 9, description: 'alguna descripcion3'
-    }
-]
-
 router.get('/', (req, res) => {
     res.render('index', {
       title: 'Mercadito Fabu',
